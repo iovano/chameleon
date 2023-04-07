2ea9f1f8392b563f3b1969eba6f1e9806cc7d49b
Timor Kodal <timor@kodal.de>
2023-04-07 18:21:38 +0200

    implement infoOverlay

--- public/js/chameleon.js
+++ public/js/chameleon.js
@@ -1,7 +1,11 @@
 import DotGain from './classes/DotGain.js';
 import Gallery from './classes/Gallery.js'
 let gallery;
-let images = ['images/example.png','images/example2.jpg','images/example3.jpg'];
+let images = [
+  {src: 'images/example.png', title: '<h2>Hohe Tatra</h2>', 'location': 'High Tatra | Slovakia', 'date': '2021-07-06'},
+  {src: 'images/example2.jpg', title: '<h2>Monteverde</h2><h3></h3>', 'location': 'Gomera | Canary Islands'},
+  {src: 'images/example3.jpg'}
+  ];
   function start(theme) {
     const canvas = document.getElementById("gallery");
     if (gallery) {
@@ -13,6 +17,57 @@ let images = ['images/example.png','images/example2.jpg','images/example3.jpg'];
       gallery = new DotGain(canvas);
     }
     gallery.addImages(images);
+    // in order to listen to all events from the gallery, you can implement "eventHandler" and digest events yourself:
+    // gallery.eventHandler = (event, payload, args) => {console.log(event);console.log(payload);console.log(args);}
+    
+    // in order to digest particular events, you can do so by implementing a custom callback function which will be called when a certain event has been dispatched, for example:
+    // gallery.onNavigation(event);
+    // gallery.onImageLoad(event);
+    // gallery.onTransisitionEnd(image);
+    // gallery.onCanvasCreated(canvasObject);
+    // etc.
+    function onImageLoad(payload, ...args) {
+      document.querySelectorAll('.infoOverlay').forEach((element) => {
+        element.classList.add('hide');
+      })
+      setTimeout(() => showImageInfo(payload, args), 2000);
+    }
+    function showImageInfo(payload, ...args) {
+      document.querySelectorAll('.infoOverlay').forEach((element) => {
+        element.classList.remove('hide');
+      })
+      document.querySelectorAll('.infoOverlay .imageInfo').forEach((element) => {
+        element.classList.remove('hide');
+        let list = {...payload.image};
+        delete list?.src;
+        if (typeof list === 'string' || list instanceof String || Object.keys(list).length === 0) {
+            element.replaceChildren(document.createElement('span').innerHTML = 'No Information available');
+          } else {
+            let ul = document.createElement('ul');
+            for (let key in list) {
+              let li = document.createElement('li');
+              li.classList.add(key);
+              let div = document.createElement('div');
+              div.classList.add('label');
+              let span = document.createElement('span');
+              span.innerHTML = key;
+              div.appendChild(span);
+              li.appendChild(div);
+              div = document.createElement('div');
+              div.classList.add('value');
+              span = document.createElement('span');
+              span.innerHTML = payload.image[key];
+              div.appendChild(span);
+              li.appendChild(div);
+              ul.appendChild(li);  
+            }
+            element.replaceChildren(ul);
+          }
+        }
+      );
+    }
+    gallery.onImageLoad = onImageLoad;
+
     gallery.direction = "random";
     gallery.init();
     gallery.run();
@@ -29,6 +84,6 @@ let images = ['images/example.png','images/example2.jpg','images/example3.jpg'];
     );
     document.querySelectorAll('.controls .button').forEach(
       button =>
-        button.addEventListener("click", () => move(button.dataset.add))
+        button.addEventListener("click", () => move(button.dataset.target))
     );
   });
