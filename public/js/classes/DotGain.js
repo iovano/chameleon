2b1b12cad7efaa5daf144d497479cbc248fbdf0b
Timor Kodal <timor@kodal.de>
2023-04-08 10:32:50 +0200

    outsource essential functionality to base class

--- public/js/classes/DotGain.js
+++ public/js/classes/DotGain.js
@@ -7,24 +7,8 @@ class DotGain extends Gallery {
     clipPath = undefined;
     clipPathId = "clipPathMask";
     maskedImage = undefined;
-    canvasContainer = undefined;
     debugMask = false;
-    imageSlots = ["currentImage", "previousImage"]
-    direction = "random";
-    alignImages = {x: 0.5, y: 0.5}; // 0 = left, 0.5 = center, 1 = right
 
-    init() {
-        super.init();
-        this.createCanvas();
-        this.navigate(0);
-    }
-    update() {
-        super.update();
-        this.updateClipPathTransition();
-        if (this.idleTime > this.duration && !this.suspended) {
-            this.navigate("+1");
-        }
-    }
     updateClipPathTransition() {
         let parameters = {
             frame: this.frame -1, 
@@ -38,71 +22,13 @@ class DotGain extends Gallery {
         this.dispatchEvent('onTransitionStart', parameters);
         if (!parameters.suspended && parameters.y < parameters.limitY) {
             parameters.y = parameters.limitY;
+            this._onTransitionEnd();
             if (parameters.idleTime === undefined) {
                 document.getElementById('imageGroup1').removeAttributeNS(null, "clip-path");
-                this.dispatchEvent("onTransitionEnd", {image: this.getCurrentImage()});
-                this.idleTime = 0;
             }
         }
         this.clipPath.style.transform = "rotate("+parameters.direction+"deg) translate("+parameters.x+"px, "+parameters.y+"px)";
 
-    }
-    navigate(delta = undefined) {
-        this.dispatchEvent("onNavigation", {target: delta});
-        super.navigate(delta);
-        this.suspended = true;
-        this.frame = 0;
-        this.idleTime = undefined;
-        this.currentDirection = (this.direction === 'random' ? Math.random() * 360 : this.direction);
-        this.updateClipPathTransition();
-        this.showImage();
-    }
-    getImageSlot(index = undefined) {
-        return document.getElementById("imageSlot" + (index !== undefined ? index : (this.imageSlots.length - 1)));
-    }
-    /* internal event listeners */
-    _onImageLoad(event, image) {
-        if (event.target === this.getImageSlot(1)) {
-            /* only dispatch event */
-            this.dispatchEvent("onImageLoad", {event: event, image: image});
-        }
-        if (this.canvas instanceof HTMLCanvasElement) {
-            const ctx = this.canvas.getContext("2d");
-            ctx.drawImage(event.target,0,0);
-        } else if (this.canvas instanceof HTMLElement) {
-            event.target.setAttributeNS(null, "x", (this.width - event.target.getBBox().width) * this.alignImages.x);
-            event.target.setAttributeNS(null, "y", (this.height - event.target.getBBox().height) * this.alignImages.y);
-
-            if (event.target === this.getImageSlot(1)) {
-                /* active/current image (foreground) */
-                if (!this.debugMask) {
-                    document.getElementById('imageGroup1').setAttributeNS(null, "clip-path", "url(#"+this.clipPathId+")");
-                }
-                this.getImageSlot(1).setAttributeNS(null, "visibility", "visible");
-                this.suspended = false;
-            } else if (event.target === this.getImageSlot(0)) {
-                /* inactive/previous image (background) */
-                let img = this.getCurrentImage();
-                this.getImageSlot(1).setAttributeNS(null, "href", img?.src || img);    
-                if (!this.debugMask) {
-                    document.getElementById('imageGroup1').setAttributeNS(null, "clip-path", "url(#"+this.clipPathId+")");
-                }
-                this.getImageSlot(1).setAttributeNS(null, "visibility", "visible");
-            }
-        }
-    }
-    showImage(index = undefined) {
-        let current = this.getImageSlot(1);
-        let previous = this.getImageSlot(0);
-        if (current.getAttributeNS(null, "href")) {
-            /* deactivate clip path for foreground image */            
-            document.getElementById('imageGroup1').removeAttributeNS(null, "clip-path");
-            /* copy previous image from main to background image slot */
-            previous.setAttributeNS(null, "href", current.getAttributeNS(null, "href"));
-        } else {
-            let img = this.getCurrentImage();
-            current.setAttributeNS(null, "href", img?.src || img);    
-        }
     }
     createClipPath(clipPathId = undefined) {
         let clipPath = document.createElementNS(this.svgNS, this.debugMask ? "g" : "clipPath");
@@ -153,63 +79,6 @@ class DotGain extends Gallery {
             clipPath.id = this.clipPathId || clipPathId;
         }
         return clipPath;
-    }
-    createCanvas() {
-        let w = this.width;
-        let h = this.height;
-        let context = this.canvas;
-
-        /* create svg object which contains dotGain - mask */
-        let svg = document.createElementNS(this.svgNS, "svg");
-        svg.setAttribute("viewBox", "0 0 "+w+" "+h);
-
-        let clipPath = this.createClipPath();
-
-
-        for (let i=0;i<this.imageSlots.length;i++) {
-            /* create image group (containing image + background) for each image slot */
-            let imageGroup = document.createElementNS(this.svgNS, "g");
-            imageGroup.id = 'imageGroup'+i;
-            imageGroup.setAttributeNS(null, 'class', 'imageGroup');
-
-            /* create placeholder image element */
-            let image = document.createElementNS(this.svgNS, "image");            
-            image.id = 'imageSlot'+i;
-            image.setAttributeNS(null, "visibility", "visible");
-            /* assign internal onload - handler for image */
-            image.onload = (event) => this._onImageLoad(event, this.getCurrentImage());
-
-            /* create background */
-            let background = document.createElementNS(this.svgNS, "rect");
-            background.setAttributeNS(null, 'width', this.width);
-            background.setAttributeNS(null, 'height', this.height);
-            background.setAttributeNS(null, 'fill', 'black');
-
-            /* append elements to svg canvas */
-            imageGroup.appendChild(background);
-            imageGroup.appendChild(image);
-            svg.appendChild(imageGroup);
-        }
-
-        /* svg canvas layout */
-        svg.style.position = "absolute";
-        svg.style.zIndex = 10;
-
-        /* append clipPath */
-        svg.appendChild(clipPath);
-
-        this.dispatchEvent("onCanvasCreated", {canvas: svg, clipPath: clipPath, context: context});
-
-        this.canvasContainer = svg;
-        if (context instanceof HTMLCanvasElement) {
-            context.getContext("2d").drawImage(svg,0,0);
-        } else {
-            /* create div element, append svg canvas container and add it to gallery context element */
-            let div = document.createElement('div')
-            div.appendChild(svg);
-            context.appendChild(div);
-            this.clipPath = clipPath;
-        }
     }
 }
 export default DotGain;
--- public/js/classes/Gallery.js
+++ public/js/classes/Gallery.js
@@ -2,6 +2,7 @@ class Gallery {
     svgNS = "http://www.w3.org/2000/svg";
     images = [];
     loadedImages = [];
+    imageSlots = ["currentImage", "previousImage"]
     previousImage = undefined;
     currentImageNum = 0;
     duration = 200; /* slideshow duration in frames */
@@ -13,22 +14,35 @@ class Gallery {
     lazyLoad = false;
     suspended = false;
     mask = undefined;
+    canvasContainer = undefined;
+    direction = "random";
+    alignImages = {x: 0.5, y: 0.5}; // 0 = left, 0.5 = center, 1 = right
+
     fps = 20; /* canvas update frequency (frames per second) */
     constructor(canvas, images = undefined, width = undefined, height = undefined) {
         this.canvas = canvas;
-        this.width = width || this.canvas.clientWidth || this.canvas.width || 800;
-        this.height = height || this.canvas.clientHeight || this.canvas.height || 600;
+        this.width = width || this.canvas.clientWidth || this.canvas.width || document.clientWidth || 800;
+        this.height = height || this.canvas.clientHeight || this.canvas.height || document.clientHeight || 600;
         console.debug("canvas size: "+(this.width + "x" + this.height));
         if (images) {
             this.addImages(images);
         }
     }
+    destroy() {
+        this.canvas.removeChild(this.canvasContainer);
+        this.canvas = undefined;
+        this.run = undefined;
+    }
     navigate(target = "+1") {
         /* use delta if target contains a signed value (e.g. "+1" or "-10"), use absolute value if target is unsigned or increase by 1 if target is undefined */
         this.setCurrentImageNum(parseInt(target) + ((typeof target === 'string' || target instanceof String) && ["+","-"].indexOf((target || 0).substring(0,1) !== -1) ? this.getCurrentImageNum() : 0));
-        if (this.onNavigate) {
-            this.onNavigate(delta);
-        }
+        this.dispatchEvent("onNavigation", {target: target});
+        this.suspended = true;
+        this.frame = 0;
+        this.idleTime = undefined;
+        this.currentDirection = (this.direction === 'random' ? Math.random() * 360 : this.direction);
+        this.updateClipPathTransition();
+        this.showImage();
     }
     getCurrentImage() {
         return this.images[this.currentImageNum];
@@ -50,12 +64,22 @@ class Gallery {
         this.height = height;
     }
     init() {
-//        this.showImage();
+        this.createCanvas();
+        this.navigate(0);
     }
     update() {
         if (!this.suspended) {
             this.frame ++;
             if (this.idleTime !== undefined) this.idleTime ++;
+        }
+        this.updateClipPathTransition();
+        if (this.idleTime > this.duration && !this.suspended) {
+            this.navigate("+1");
+        }
+    }
+    updateClipPathTransition() {
+        if (this.idleTime === undefined) {
+            this._onTransitionEnd();
         }
     }
     dispatchEvent(eventName, payload, ...args) {
@@ -79,7 +103,13 @@ class Gallery {
         }
         this.images = images;
     }
-    /* internal event listener */
+    /* internal event listeners */
+    _onTransitionEnd() {
+        if (this.idleTime === undefined) {
+            this.dispatchEvent("onTransitionEnd", {image: this.getCurrentImage()});
+            this.idleTime = 0;
+        }
+    }
     _onImageLoad(event, image = null) {
         this.dispatchEvent("onImageLoad", {event: event, image: image});
         if (this.canvas instanceof HTMLCanvasElement) {
@@ -107,8 +137,106 @@ class Gallery {
             return img;
         }
     }
-    createCanvas() {
+    getImageSlot(index = undefined) {
+        return document.getElementById("imageSlot" + (index !== undefined ? index : (this.imageSlots.length - 1)));
+    }
+    /* internal event listeners */
+    _onImageLoad(event, image) {
+        if (event.target === this.getImageSlot(1)) {
+            /* only dispatch event */
+            this.dispatchEvent("onImageLoad", {event: event, image: image});
+        }
+        if (this.canvas instanceof HTMLCanvasElement) {
+            const ctx = this.canvas.getContext("2d");
+            ctx.drawImage(event.target,0,0);
+        } else if (this.canvas instanceof HTMLElement) {
+            event.target.setAttributeNS(null, "x", (this.width - event.target.getBBox().width) * this.alignImages.x);
+            event.target.setAttributeNS(null, "y", (this.height - event.target.getBBox().height) * this.alignImages.y);
 
+            if (event.target === this.getImageSlot(1)) {
+                /* active/current image (foreground) */
+                if (!this.debugMask) {
+                    document.getElementById('imageGroup1').setAttributeNS(null, "clip-path", "url(#"+this.clipPathId+")");
+                }
+                this.getImageSlot(1).setAttributeNS(null, "visibility", "visible");
+                this.suspended = false;
+            } else if (event.target === this.getImageSlot(0)) {
+                /* inactive/previous image (background) */
+                let img = this.getCurrentImage();
+                this.getImageSlot(1).setAttributeNS(null, "href", img?.src || img);    
+                if (!this.debugMask) {
+                    document.getElementById('imageGroup1').setAttributeNS(null, "clip-path", "url(#"+this.clipPathId+")");
+                }
+                this.getImageSlot(1).setAttributeNS(null, "visibility", "visible");
+            }
+        }
+    }
+    showImage(index = undefined) {
+        let current = this.getImageSlot(1);
+        let previous = this.getImageSlot(0);
+        if (current.getAttributeNS(null, "href")) {
+            /* deactivate clip path for foreground image */            
+            document.getElementById('imageGroup1').removeAttributeNS(null, "clip-path");
+            /* copy previous image from main to background image slot */
+            previous.setAttributeNS(null, "href", current.getAttributeNS(null, "href"));
+        } else {
+            let img = this.getCurrentImage();
+            current.setAttributeNS(null, "href", img?.src || img);    
+        }
+    }
+    createCanvas() {
+        let w = this.width;
+        let h = this.height;
+        let context = this.canvas;
+        let clipPath = null;
+
+        /* create svg object which contains dotGain - mask */
+        let svg = document.createElementNS(this.svgNS, "svg");
+        svg.setAttribute("viewBox", "0 0 "+w+" "+h);
+
+        for (let i=0;i<this.imageSlots.length;i++) {
+            /* create image group (containing image + background) for each image slot */
+            let imageGroup = document.createElementNS(this.svgNS, "g");
+            imageGroup.id = 'imageGroup'+i;
+            imageGroup.setAttributeNS(null, 'class', 'imageGroup');
+
+            /* create placeholder image element */
+            let image = document.createElementNS(this.svgNS, "image");            
+            image.id = 'imageSlot'+i;
+            image.setAttributeNS(null, "visibility", "visible");
+            /* assign internal onload - handler for image */
+            image.onload = (event) => this._onImageLoad(event, this.getCurrentImage());
+
+            /* create background */
+            let background = document.createElementNS(this.svgNS, "rect");
+            background.setAttributeNS(null, 'width', this.width);
+            background.setAttributeNS(null, 'height', this.height);
+            background.setAttributeNS(null, 'fill', 'black');
+
+            /* append elements to svg canvas */
+            imageGroup.appendChild(background);
+            imageGroup.appendChild(image);
+            svg.appendChild(imageGroup);
+        }
+
+        if (this.createClipPath) {
+            clipPath = this.createClipPath();
+            /* append clipPath */
+            svg.appendChild(clipPath);
+            this.clipPath = clipPath;
+        }
+
+        this.dispatchEvent("onCanvasCreated", {canvas: svg, clipPath: clipPath, context: context});
+
+        if (context instanceof HTMLCanvasElement) {
+            context.getContext("2d").drawImage(svg,0,0);
+        } else {
+            /* create div element, append svg canvas container and add it to gallery context element */
+            let div = document.createElement('div')
+            div.appendChild(svg);
+            context.appendChild(div);
+            this.canvasContainer = div;
+        }
     }
 }
 export default Gallery;
