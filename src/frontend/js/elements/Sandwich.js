d4b871aad3831110f4df080a09aec21b1dba5fe7
Timor Kodal <timor@kodal.de>
2023-04-24 20:52:12 +0200

    implement sandwich navigation

--- src/frontend/js/elements/Sandwich.js
+++ src/frontend/js/elements/Sandwich.js
@@ -6,6 +6,9 @@ export default class Sandwich extends HTMLElement {
     target = undefined;
     title = undefined;
     expanded = false;
+    items = [];
+    defaultMenuItemNum = undefined;
+    selectedMenuItemNum = undefined;
     constructor(menu = undefined) {
         super();
         this.menu = menu;
@@ -31,8 +34,16 @@ export default class Sandwich extends HTMLElement {
                 this.menu.classList.add(this.attributes.menuClass.value.split(" "));
             }
             this.appendChild(this.menu);
+        } else {
+            this.menu.querySelectorAll('li').forEach((element) => {
+                let idx = this.items.push(element);
+                element.addEventListener('click', (event) => this._onClickMenuItem(idx - 1));
+                element.style = 'cursor: pointer';
+                }   
+            );
         }
         this.createMenuItems();
+        console.log(this.items);
         this._onToggle(false);
     }
     createMenuItems() {
@@ -46,18 +57,29 @@ export default class Sandwich extends HTMLElement {
                         li.innerHTML = items[i];
                     } else {
                         li.innerHTML = items[i].title;
+                        for(let ki of Object.keys(items[i])) {
+                            li.dataset[ki] = items[i][ki];
+                        }
                     }
                     li.style = 'cursor: pointer';
-                    li.addEventListener('click', (event) => this._onClickMenuItem(event, items[i]));
                     this.menu.appendChild(li);
+                    let idx = this.items.push(li) - 1;
+                    li.addEventListener('click', (event) => this._onClickMenuItem(idx));
+                    if (items[i].default === true) {
+                        this.defaultMenuItemNum = idx;
+                        this.selectMenuItemNum(idx);
+                    }
                 }    
             }
-            console.log(this.menu);
+            console.log(this.items);
         }
     }
-    _onClickMenuItem(event, item) {
-        let page = (item instanceof Object) ? item.link : "/pages/"+item.toLowerCase()+".html";
-        console.log("loading", item, page);
+    selectMenuItemNum(itemNum) {
+        itemNum = itemNum !== undefined ? itemNum : this.selectMenuItemNum;
+        let item = this.items[itemNum];
+        let page = item.dataset?.link;
+        console.log("selected", itemNum, item, page);
+        if (!page) page = "/pages/"+item.innerHTML.toLowerCase()+".html";
         if (this.target) {
             fetch(page).then((response) => {return response.text();}).then((html) => {
                 // Convert the HTML string into a document object
@@ -70,7 +92,12 @@ export default class Sandwich extends HTMLElement {
                 console.error("error loading ressource",item,error);
             });
         }
-        console.log("menu item clicked", item);
+        this.items[this.selectedMenuItemNum]?.classList.remove('active');
+        this.selectedMenuItemNum = itemNum;
+        this.items[this.selectedMenuItemNum].classList.add('active');
+    }
+    _onClickMenuItem(itemNum) {
+        this.selectMenuItemNum(itemNum);
     }
     _onToggle(expanded = undefined) {
         this.expanded = (expanded !== undefined ? expanded : !this.expanded);
@@ -89,9 +116,6 @@ export default class Sandwich extends HTMLElement {
         }
         this.style.pointerEvents = this.expanded ? 'all' : 'none';
 
-        if (!this.expanded) {
-            this.target.innerHTML = '';
-        }
         if (this.onToggle) {
             this.onToggle(this.expanded);
         }
